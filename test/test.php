86eafa3d9f80f81b89514a8b65e15e9657f72c57
Masaki Kawabata Neto <kawabata@usp.br>
2019-05-29 11:34:52 -0300

    melhoria nos testes

--- test/test.php
+++ test/test.php
@@ -1,8 +1,13 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+
+// não vamos usar o autoload aqui para ficar compativel com clone ou composer
+require __DIR__ . '/../src/Cutter.php';
 
 use Uspdev\Cutter;
 
+// Se quiser usar a lista original troque nomes.csv por /../src/cutter.csv.
+// Para rodar ele vai demorar um tanto então não tenha pressa
+#$arr = Cutter::load(__DIR__ . '/../src/cutter.csv');
 $arr = Cutter::load(__DIR__ . '/nomes.csv');
 
 $ok = 0;
@@ -10,6 +15,9 @@ $erro = 0;
 
 echo 'Vamos pegar a lista conhecida "nomes.csv" e verificar os seus códigos cutter' . PHP_EOL;
 
+$count = count($arr);
+$i = 1;
+$err = '';
 foreach ($arr as $tuple) {
 
     $ret = Cutter::find($tuple[1]);
@@ -17,9 +25,12 @@ foreach ($arr as $tuple) {
         $ok++;
     } else {
         $erro++;
-        echo '"' . $tuple[1] . '" deveria ser ' . $tuple[0] . ' porém retornou ' . $ret . "\n";
+        $err .= $erro.': "' . $tuple[1] . '" deveria ser ' . $tuple[0] . ' porém retornou ' . $ret . "\n";
     }
+    echo 'Testando ' . $i . ' de ' . $count . PHP_EOL. "\033[1A";
+    $i++;
 }
-
-echo 'Testes ok: ' . $ok . "\n";
-echo 'Testes com erro: ' . $erro . "\n";
+echo "\n";
+echo 'Testes ok: ' . $ok . PHP_EOL;
+echo 'Testes com erro: ' . $erro . PHP_EOL;
+echo $err ? $err . PHP_EOL: '';
